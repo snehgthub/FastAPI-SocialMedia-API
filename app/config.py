23a9fe6dadd2f709e74982de0f4f430f060e8a83
Sneh Chauhan <srchauhan.ind@gmail.com>
2024-04-07 23:03:29 +0530

    Removed code for systemd management

--- app/config.py
+++ app/config.py
@@ -1,19 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import SecretStr, validator
-import os
-
-
-def load_secret(secret_name: str) -> SecretStr:
-    # Try to load from the systemd secure directory first
-    credentials_dir = os.getenv("CREDENTIALS_DIRECTORY")
-    if credentials_dir:
-        secret_file_path = os.path.join(credentials_dir, secret_name)
-        with open(secret_file_path, "r") as file:
-            secret_value = file.read().strip()
-        return SecretStr(secret_value)
-    else:
-        # Fallback to loading from environment variables
-        return SecretStr(os.getenv(secret_name.upper(), ""))
+from pydantic import SecretStr
 
 
 class Settings(BaseSettings):
@@ -27,14 +13,6 @@ class Settings(BaseSettings):
     secret_key: SecretStr
     algorithm: str
     access_token_expire_minutes: int
-
-    @classmethod
-    @validator("*")
-    def load_secrets_if_needed(cls, v, values):
-        for field_name, field in cls.__fields__.items():
-            if field.type_ is SecretStr and field_name in values:
-                values[field_name] = load_secret(field_name)
-        return v
 
 
 settings = Settings()  # type: ignore
